d2ce9edaebfd7659b65decdc073a686e3aa2b872
Tomas Sirio <tsirio@bol.com>
2023-11-10 14:02:34 +0100

    First commit

---
+++ src/test/java/com/tekal/elevatortechtest/ElevatorTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekal.elevatortechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorTechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c6f700a96a879cf423e880c42ed162756d223f4e
Tomas Sirio <tsirio@bol.com>
2023-11-11 12:31:38 +0100

    Add async logic to Elevator Service. Configure Async ThreadPool

--- src/main/java/com/tekal/elevatortechtest/service/ElevatorService.java
+++ src/main/java/com/tekal/elevatortechtest/service/ElevatorService.java
@@ -1,8 +1,10 @@
 package com.tekal.elevatortechtest.service;
 
 import com.tekal.elevatortechtest.model.request.ElevatorCall;
+import org.springframework.scheduling.annotation.Async;
 
 public interface ElevatorService {
+    @Async("elevatorTaskExecutor")
     void processElevatorCall(ElevatorCall elevatorCall);
 
 }
